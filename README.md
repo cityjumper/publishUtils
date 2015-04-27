742385d08b83c12c2848b6c1a6337c7c033abe82
Gareth <gareth.bj.thomas@gmail.com>
2015-04-27 19:30:50 +0200

    Fixes #8
    Cleaned documentation and finished the MATLAB Toolbox layout

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ publishUtils
 Given the usefulness of MATLAB and the publish command, this repository aims to add more rich HTML features, when using publish command.
 
 To get started:
->> run_this
+- Double Click on Publish Utils Toolbox.mltbx
+- open your Documentation and look for examples under Suplmental Software.
--- Source/help/functionlist.m
+++ Source/help/functionlist.m
@@ -3,6 +3,9 @@
 % that these functions are simple wrapper around the Google Chart API. <https://developers.google.com/chart/ Google Charts API>
 %%
 
+% to use all of these one needs to type: disp(make*(inputs));
+
+
 %% makeTableChart
 % Input: table data type from MATLAB
 
@@ -11,3 +14,21 @@
 % Input: table data type from MATLAB. This table should have first column
 % names, and the second numbers
 
+%% makeGeoChart
+% Input: table data type from MATLAB. The second input has to do with the
+% Geo Chart Options. It is a structure which has myChartOptions.region,myChartOptions.displaymode
+
+%% makeMap
+% Input: table data type from MATLAB. This table should have first column
+% names of countries/places, and the second numbers
+
+%% makeLineChart
+% Input: table data type from MATLAB. The second input has a strucuture
+% like: myChartOptions.isStacked, to enable the stacked chart or not.
+
+%% makeColumnChart
+% Input: table data type from MATLAB. The second input has a strucuture
+% like: myChartOptions.isStacked, to enable the stacked chart or not.
+
+%% makeAnnotationChart
+% Input: table data type from MATLAB.
